37e4f1545b09a0d50560262e7a57c7eff9071d3a
Nogura <resist_network@protonmail.com>
2018-12-28 02:26:36 -0500

    Testing....

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+//Resist.Network Theme Functions
+if ( ! function_exists ( 'one_page_express_copyright' ) ) {
+    function one_page_express_copyright() {
+        // Contents of your function here.
+       return '&copy;2019 Resist.Network | All Rights Reserved';
+    }
+}
