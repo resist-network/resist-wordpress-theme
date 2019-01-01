11fe67cabf63cfeeb91349a42c4a5a1a4d48af54
system-operator <mindamp@gmail.com>
2018-12-31 22:31:27 -0600

    Bleh... automation and stuff.

--- functions.php
+++ functions.php
@@ -14,4 +14,33 @@ function resist_network_override_avatar ($avatar_html, $id_or_email, $size, $def
     return $new_avatar_html;
 }
 add_filter ('get_avatar', 'resist_network_override_avatar', 10, 5);
+//Customizes the WooCommerce Loading Animation
+add_action('wp_head', 'custom_ajax_spinner', 1000 );
+function custom_ajax_spinner() {
+    ?>
+    <style>
+    .woocommerce .blockUI.blockOverlay:before,
+    .woocommerce .loader:before {
+        height: 3em;
+        width: 3em;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-left: -.5em;
+        margin-top: -.5em;
+        display: block;
+        content: "";
+        -webkit-animation: none;
+        -moz-animation: none;
+        animation: none;
+        background-image:url('<?php echo get_stylesheet_directory_uri() . "/images/laoding.gif"; ?>') !important;
+        background-position: center center;
+        background-size: cover;
+        line-height: 1;
+        text-align: center;
+        font-size: 2em;
+    }
+    </style>
+    <?php
+}
 //More to come...
